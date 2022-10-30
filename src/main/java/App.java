469d1527ec0777f65b84d3445b503ee8fd4f1588
Sergei <395105@mail.ru>
2022-10-30 19:30:00 +0400

    Автотест пройден

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,31 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.SortedMap;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        System.out.println(bean.equals(bean2));
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getMessage());
+
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4.getMessage());
+
+        System.out.println(bean3.equals(bean4));
+
     }
 }
